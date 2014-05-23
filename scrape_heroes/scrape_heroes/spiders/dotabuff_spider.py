8d9528ee6e287d652557086f949724f2831d6540
Sina Tamanna <sina@sina-desktop.(none)>
2014-05-23 17:35:56 +0200

    better structure of the hero matchup data

--- scrape_heroes/scrape_heroes/spiders/dotabuff_spider.py
+++ scrape_heroes/scrape_heroes/spiders/dotabuff_spider.py
@@ -3,12 +3,9 @@ from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.selector import Selector
 from scrape_heroes.items import HeroItem
 from selenium import webdriver
-
 from itertools import izip
-from collections import namedtuple
+from utils.utilts import Matchup
 import time
-
-Info = namedtuple('Info', 'advantage, win_rate, number_of_matches')
 
 
 class DotaBuffSpider(CrawlSpider):
@@ -22,7 +19,7 @@ class DotaBuffSpider(CrawlSpider):
                                           'economy', 'farm', 'damage',
                                           'trends', 'abilities', 'builds', 'items', 'skills'
                                           'http://dotabuff.com/heroes/[\w+]+[-\w+]*/matchups',
-                                          'http://\w+.dotabuff.com'])),]
+                                          'http://\w+.dotabuff.com'])), ]
 
     def __init__(self):
         CrawlSpider.__init__(self)
@@ -42,7 +39,7 @@ class DotaBuffSpider(CrawlSpider):
             number_of_matches = table_selector.xpath('.//tr[*]/td[5]/div[1]/text()').extract()
 
             for n, a, wr, nom in izip(name, advantage, win_rate, number_of_matches):
-                table_data[n] = Info(a, wr, nom)
+                table_data[n] = Matchup(a, wr, nom)
 
             return table_data
 
--- suggest/suggest.py
+++ suggest/suggest.py
@@ -1,34 +1,43 @@
+from pprint import pprint
+from utils.utils import Matchup, Counter
 import json
 import re
-from pprint import pprint
 
 
 class Suggest(object):
     def __init__(self):
-        self.clear_name = lambda s: s.lower().replace('-', '')
-        """ read matchup information from file that is a list of dictionaries 
-            with each dictionary holding information about a hero """
+        self.clear_name = lambda s: s.lower().replace('-', '').replace(' ', '')
+        self.extract_float = lambda s: float(re.match("\-?\d+.\d+", s).group())
         self.matchups = {}
+
+        """ read matchup information from file that is a list of dictionaries 
+            with each dictionary holding information about a hero matchups
+            [{'name': 'zeus', 'matchups': {'Abaddon': ['-1.14%', '47.29%', '116,531'], ...}}, {...}] """
         with open("../heroes_json/heroes.json", 'r') as f:
             for hero_matchup in json.load(f):
                 name = self.clear_name(hero_matchup['name'])
-                self.matchups.update({name: hero_matchup['matchups']})
+                matchups = {n: Matchup(*m) for n, m in hero_matchup['matchups'].iteritems()}
+                self.matchups.update({name: matchups})
 
     def _print_matchups(self):
         pprint(self.matchups)
 
+    def _print_counter(self, pick, counter_pick):
+        message = "Best counter to {p} is {cn} with advantage of {a} and win_rate of {wr} of {nom} games"
+        cn = counter_pick.name
+        cm = counter_pick.matchup
+        print(message.format(p=pick, cn=cn, a=self.extract_float(cm.advantage)*(-1), wr=cm.win_rate, nom=cm.number_of_matches))
+
     def find_best_counter(self, hero_name):
+        """ Searches through all the matchup information of every hero and selects the one that
+            the current hero has the highest disadvantage against """
+
         hero_name = self.clear_name(hero_name)
+        """ Dictionary of {'hero_name': Matchup} """
         matchup = self.matchups[hero_name]
-        best_counter_name = ''
-        best_counter_advantage = 1000.0
 
-        for hero_name, matchup in matchup.iteritems():
-            advantage = float(re.match("\-?\d+.\d+", matchup[0]).group())
-            if advantage < best_counter_advantage:
-                best_counter_advantage = advantage
-                best_counter_name = hero_name
+        min_advantage = lambda x: self.extract_float(x[1].advantage)
+        counter_pick = Counter(*min(matchup.iteritems(), key=min_advantage))
 
-        print best_counter_name, best_counter_advantage
-
+        self._print_counter(hero_name, counter_pick)
 
